6007267aef27a8de4d37919b8e19e73858d4354e
daosgava <daosgava.garcia@gmail.com>
2025-09-22 18:03:10 -0500

    Initial commit

---
+++ public/js/algorithms/depthFirst.js
@@ -0,0 +1,60 @@
+const createHtmlElem = (tag, cssClasses) => {
+  const elem = document.createElement(tag);
+  cssClasses.forEach((cssClass) => elem.classList.add(cssClass));
+
+  return elem;
+};
+
+const createNodeContainer = (node) => {
+  const nodeElem = createHtmlElem("div", ["node"]);
+  const valueElem = createHtmlElem("div", ["value"]);
+  valueElem.innerText = node.value;
+
+  const valueContainerElem = createHtmlElem("div", ["value-container"]);
+
+  valueContainerElem.appendChild(valueElem);
+  nodeElem.appendChild(valueContainerElem);
+
+  return nodeElem;
+};
+
+const appendNodeToPage = (targetContainer, node) => {
+  const nodeElem = createNodeContainer(node);
+  targetContainer.appendChild(nodeElem);
+  targetContainer.classList.add("parent");
+
+  return nodeElem;
+};
+
+const addSeparator = (nodeElem, node) => {
+  if (node.left || node.right) {
+    const separator = createHtmlElem("div", ["separator"]);
+    const vline = createHtmlElem("div", ["vline"]);
+    const hline = createHtmlElem("div", ["hline"]);
+    separator.appendChild(vline);
+    separator.appendChild(hline);
+    nodeElem.appendChild(separator);
+  }
+};
+
+const drawNode = (node, htmlContainer) => {
+  const nodeElem = appendNodeToPage(htmlContainer, node);
+  addSeparator(nodeElem, node);
+
+  return nodeElem;
+};
+
+// Depth-First
+export const depthFirst = (node, htmlContainer, level = 0) => {
+  if (!node?.value) return false;
+
+  node.level = level;
+  const nextLevel = level + 1;
+
+  const nodeElem = drawNode(node, htmlContainer);
+
+  return (
+    depthFirst(node.left, nodeElem, nextLevel) ||
+    depthFirst(node.right, nodeElem, nextLevel)
+  );
+};
---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+import { TreeNode } from "./structures/tree.js";
+import { depthFirst } from "./algorithms/depthFirst.js";
+
+const initializeTree = () => {
+  const root = new TreeNode(1);
+
+  root.left = new TreeNode(2);
+  root.right = new TreeNode(3);
+  root.left.left = new TreeNode(4);
+  root.left.right = new TreeNode(5);
+  root.right.left = new TreeNode(6);
+  root.right.right = new TreeNode(7);
+  root.right.right.left = new TreeNode(8);
+  root.right.right.left.left = new TreeNode(9);
+  root.right.right.left.right = new TreeNode(11);
+  root.right.right.right = new TreeNode(10);
+
+  const rootContainer = document.querySelector("#tree");
+  depthFirst(root, rootContainer);
+};
+
+initializeTree();
