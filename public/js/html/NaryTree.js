5bc2301f0f315ad7f356ca57baebab0ca5b7e3eb
daosgava <daosgava.garcia@gmail.com>
2025-10-12 19:10:19 -0500

    Add connect node method to NaryTree

--- public/js/html/NaryTree.js
+++ public/js/html/NaryTree.js
@@ -1,6 +1,7 @@
 import { Tree } from "./Tree.js";
 import { Node } from "./Node.js";
 import { createSubNaryTreeElem } from "../helpers/element-factory/naryTree.js";
+import { createHtmlElem } from "../helpers/element-factory/generic.js";
 
 const createSubTree = (node) => {
   const { subTreeElem, rootElem, childrenContainerElem } =
@@ -9,30 +10,85 @@ const createSubTree = (node) => {
   const { nodeElem } = htmlNode.getElements();
   rootElem.appendChild(nodeElem);
 
-  return { subTreeElem, childrenContainerElem };
+  return { subTreeElem, childrenContainerElem, nodeElem };
+};
+
+const createHLine = (children) => {
+  const first = children[0]?.nodeElem;
+  const last = children[children.length - 1]?.nodeElem;
+
+  if (!first || !last) return;
+
+  const firstAnchor = window
+    .getComputedStyle(first)
+    .getPropertyValue("anchor-name");
+  const lastAnchor = window
+    .getComputedStyle(last)
+    .getPropertyValue("anchor-name");
+
+  const hLine = createHtmlElem({ tag: "div", classes: ["hLine"] });
+
+  hLine.style.insetInlineStart = `anchor(${firstAnchor} center)`;
+  hLine.style.insetInlineEnd = `anchor(${lastAnchor} center)`;
+  hLine.style.insetBlockEnd = `anchor(${firstAnchor} top)`;
+
+  return hLine;
+};
+
+const createLimb = (nodeElem, upward = false) => {
+  if (!nodeElem) return;
+  const elemAnchor = window
+    .getComputedStyle(nodeElem)
+    .getPropertyValue("anchor-name");
+
+  const limb = createHtmlElem({ tag: "div", classes: ["limb"] });
+
+  limb.style.insetInlineStart = `anchor(${elemAnchor} center)`;
+  limb.style[upward ? "inset-block-start" : "inset-block-end"] =
+    `anchor(${elemAnchor} center)`;
+
+  return limb;
 };
 
 export class NaryTree extends Tree {
   constructor(root, appContainer) {
     super(root, appContainer);
   }
-  
-  
+
+  #connectNodes(children) {
+    const hLine = createHLine(children);
+    if (hLine) this.treeContainer.appendChild(hLine);
+
+    const limbs = children.map(({ nodeElem }) => createLimb(nodeElem));
+    limbs.forEach((limb) => this.treeContainer.appendChild(limb));
+  }
 
   // Based on Breadth-First
   draw() {
-    const { subTreeElem, childrenContainerElem } = createSubTree(this.root);
+    const { subTreeElem, childrenContainerElem, nodeElem } = createSubTree(
+      this.root,
+    );
     this.appContainer.appendChild(subTreeElem);
-    const queue = [{ container: childrenContainerElem, node: this.root }];
+
+    const queue = [
+      { container: childrenContainerElem, node: this.root, nodeElem },
+    ];
 
     while (queue.length > 0) {
       const item = queue.shift();
 
+      const rootLimb = createLimb(item.nodeElem, true);
+      if (rootLimb && item.node.getChildren().length > 0)
+        this.treeContainer.appendChild(rootLimb);
+
       const children = item.node.getChildren().map((node) => {
-        const { subTreeElem, childrenContainerElem } = createSubTree(node);
+        const { subTreeElem, childrenContainerElem, nodeElem } =
+          createSubTree(node);
         item.container.appendChild(subTreeElem);
-        return { container: childrenContainerElem, node };
+        return { container: childrenContainerElem, node, nodeElem };
       });
+
+      this.#connectNodes(children);
 
       queue.push(...children);
     }
